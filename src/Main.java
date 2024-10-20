a87b585d03e32d350fedafc75800fc86b9f70b8c
marmoresimone <marmoresimone14@gmail.com>
2024-10-20 12:57:40 +0200

    test commit and push

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("questo è il progetto di ispw");
-        System.out.println("prova");
+        System.out.println("prova2");
     }
 }
