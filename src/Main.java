77ef2ed864d2b36834f64cc186ea56e8669b00a9
marmoresimone <marmoresimone14@gmail.com>
2024-10-20 13:36:18 +0200

    aggiunto nuova classe

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("questo è il progetto di ispw");
         System.out.println("prova2");
+
+        testClass pippo = new testClass(20);
+        pippo.sium();
+
     }
 }
---
+++ src/testClass.java
@@ -0,0 +1,12 @@
+public class testClass {
+    private final int peso;
+
+    public testClass(int peso) {
+        this.peso = peso;
+    }
+    public void sium(){
+        System.out.println("ciaone peso:" + peso);
+    }
+
+
+}
